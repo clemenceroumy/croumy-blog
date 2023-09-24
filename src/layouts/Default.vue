130bd88c31b82beca34f1df48a30fa870efc56fb
croumy <clemence@playmoweb.com>
2023-09-24 15:25:21 +0200

    footer: icons + links

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -51,17 +51,20 @@
       <div class="col-span-12 md:col-span-6 flex lg:justify-end items-center mt-10 md:mt-0">
         <div class="flex gap-4">
           <a href="#" class="text-text-light dark:text-text-dark">Wishlist</a>
-          <a href="#" class="text-text-light dark:text-text-dark">Faire un don</a>
+          <a href="https://ko-fi.com/croumy" class="text-text-light dark:text-text-dark" target="_blank">Faire un don</a>
         </div>
 
         <div class="bg-darkGray mx-5" style="width: 1px; height: 30px;"></div>
 
         <div class="flex gap-2">
-          <a class="mr-2" href="https://twitter.com/clemenceroumy" target="_blank">
-            <g-image src="~/assets/footer/twitter.svg" alt="twitter icon"></g-image>
+          <a class="mr-2" href="https://www.linktree.com/croumy/" target="_blank">
+            <g-image src="~/assets/footer/linktree.svg" alt="linktree icon"></g-image>
           </a>
-          <a class="mr-2" href="https://www.instagram.com/clemenceroumy/" target="_blank">
+          <a class="mr-2" href="https://www.instagram.com/croumy2/" target="_blank">
             <g-image src="~/assets/footer/insta.svg" alt="insta icon"></g-image>
+          </a>
+          <a class="mr-2" href="https://github.com/clemenceroumy" target="_blank">
+            <g-image src="~/assets/footer/github.svg" alt="github icon"></g-image>
           </a>
           <a class="mr-2" href="https://www.twitch.tv/croumy2" target="_blank">
             <g-image src="~/assets/footer/twitch.svg" alt="twitch icon"></g-image>
