7bf3a8f56b9d66b49ff9316dccd558d725df8416
croumy <clemence@playmoweb.com>
2023-09-26 16:42:36 +0200

    fix: page routing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
+    app: {
+        baseURL: '/croumy-blog/',
+        buildAssetsDir: 'assets'
+    },
     modules: [
         '@nuxtjs/tailwindcss',
         'nuxt-svgo',
         '@nuxtjs/apollo'
     ],
-    css: [
-        '@/assets/css/main.scss',
-    ],
-    svgo: {
-        autoImportPath: './assets/img/'
-    },
+    css: ['@/assets/css/main.scss',],
+    svgo: { autoImportPath: './assets/img/' },
     apollo: {
         clients: {
             default: {
--- pages/games/index.vue
+++ pages/games/index.vue
@@ -1,7 +1,5 @@
 <template>
     <div class="my-10">
-      <h1 class="header text-text-light dark:text-text-dark">JEUX</h1>
-
       <nuxt-link
           :key="game.slug"
           v-for="(game, index) in data?.games"
@@ -15,6 +13,7 @@
 
 <script lang="ts" setup>
 import ArticleShort from "~/components/article/ArticleShort.vue";
+
 import getAllGames from "~/graphql/getAllGames.graphql"
 
 const { data } = await useAsyncQuery(getAllGames);
