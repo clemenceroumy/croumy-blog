122a13b791508dd6d327937aaf12a2169d9ce8a8
croumy <clemence@playmoweb.com>
2023-09-28 13:31:11 +0200

    Updates

--- server/chunks/app/assets/Default-styles.05dc1d10.mjs
+++
@@ -1,6 +0,0 @@
-const Default_vue_vue_type_style_index_0_scoped_68ce228b_lang = "a[data-v-68ce228b],p[data-v-68ce228b]{font-size:12px}a svg[data-v-68ce228b]{height:20px;width:20px}";
-
-const DefaultStyles_05dc1d10 = [Default_vue_vue_type_style_index_0_scoped_68ce228b_lang, Default_vue_vue_type_style_index_0_scoped_68ce228b_lang];
-
-export { DefaultStyles_05dc1d10 as default };
-//# sourceMappingURL=Default-styles.05dc1d10.mjs.map
