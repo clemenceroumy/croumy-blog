2accca1ea8c9cc30665b02f9fb30939be31b503b
croumy <clemence@playmoweb.com>
2023-10-05 17:57:54 +0200

    fix: skeleton loading on collection

--- pages/collection.vue
+++ pages/collection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-10">
-    <div v-for="picture in pictures" class="group">
+    <div v-if="pictures.length > 0" v-for="picture in pictures" class="group">
       <a :href="`https://www.instagram.com/p/${picture.shortcode}/`" target="_blank" class="relative">
         <img class="rounded cursor-pointer group-hover:scale-[1.02] scale-100 transition-scale duration-200" :src="picture.path" alt="">
         <div class="absolute top-0 w-full h-full justify-center items-center bg-bg-article-dark/[.40] hidden group-hover:flex group-hover:scale-[1.02] scale-100 transition-scale duration-200">
@@ -9,6 +9,10 @@
         </div>
       </a>
     </div>
+
+    <template v-else>
+      <div v-for="_ in Array(10)" class="bg-gray w-full aspect-square rounded-md animate-pulse"></div>
+    </template>
   </div>
 </template>
 
